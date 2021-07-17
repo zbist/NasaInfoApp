05a572505452f8d8154c372c42b1e45b428b1e49
zbist <zbist@mail.ru>
2021-07-17 16:19:34 +0300

    Img Animation

--- app/src/main/java/com/zbistprod/nasainfoapp/ui/moon/MoonFragment.kt
+++ app/src/main/java/com/zbistprod/nasainfoapp/ui/moon/MoonFragment.kt
@@ -3,7 +3,13 @@ package com.zbistprod.nasainfoapp.ui.moon
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.viewModels
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.bumptech.glide.Glide
 import com.zbistprod.nasainfoapp.R
 import com.zbistprod.nasainfoapp.databinding.MoonFragmentBinding
@@ -15,6 +21,8 @@ class MoonFragment : Fragment(R.layout.moon_fragment) {
         fun newInstance() = MoonFragment()
     }
 
+    private var isExpanded = false
+
     private val viewModel: MoonViewModel by viewModels()
     private val binding: MoonFragmentBinding by viewBinding(MoonFragmentBinding::bind)
 
@@ -23,6 +31,22 @@ class MoonFragment : Fragment(R.layout.moon_fragment) {
         Glide.with(binding.moonImg)
             .load("https://trek.nasa.gov/tiles/Moon/EQ/LRO_WAC_Mosaic_Global_303ppd_v02/1.0.0/default/default028mm/0/0/1.jpg")
             .into(binding.moonImg)
+
+        binding.moonImg.setOnClickListener {
+            isExpanded = !isExpanded
+            TransitionManager.beginDelayedTransition(
+                binding.root,
+                TransitionSet().addTransition(ChangeBounds())
+                    .addTransition(ChangeImageTransform())
+            )
+
+            val params: ViewGroup.LayoutParams = binding.moonImg.layoutParams
+            params.height =
+                if (isExpanded) ViewGroup.LayoutParams.MATCH_PARENT else ViewGroup.LayoutParams.WRAP_CONTENT
+            binding.moonImg.layoutParams = params
+            binding.moonImg.scaleType =
+                if (isExpanded) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.FIT_CENTER
+        }
     }
 
 }
